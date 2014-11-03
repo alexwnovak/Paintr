1b989c929c0ad751365b3eb6982bed83d1afd83e
Alex Novak <alexwnovak@gmail.com>
2014-11-03 11:56:43 -0500

    +Menu strip with empty File menu

--- Paintr/Paintr/MainForm.Designer.cs
+++ Paintr/Paintr/MainForm.Designer.cs
@@ -28,23 +28,50 @@
       /// </summary>
       private void InitializeComponent()
       {
+         this._menuStrip = new System.Windows.Forms.MenuStrip();
+         this._fileMenu = new System.Windows.Forms.ToolStripMenuItem();
+         this._menuStrip.SuspendLayout();
          this.SuspendLayout();
+         // 
+         // _menuStrip
+         // 
+         this._menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._fileMenu});
+         this._menuStrip.Location = new System.Drawing.Point(0, 0);
+         this._menuStrip.Name = "_menuStrip";
+         this._menuStrip.Size = new System.Drawing.Size(707, 24);
+         this._menuStrip.TabIndex = 0;
+         this._menuStrip.Text = "menuStrip1";
+         // 
+         // _fileMenu
+         // 
+         this._fileMenu.Name = "_fileMenu";
+         this._fileMenu.Size = new System.Drawing.Size(37, 20);
+         this._fileMenu.Text = "&File";
          // 
          // MainForm
          // 
          this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          this.ClientSize = new System.Drawing.Size(707, 459);
+         this.Controls.Add(this._menuStrip);
+         this.MainMenuStrip = this._menuStrip;
          this.MaximizeBox = false;
          this.MinimizeBox = false;
          this.Name = "MainForm";
          this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
          this.Text = "Paintr";
+         this._menuStrip.ResumeLayout(false);
+         this._menuStrip.PerformLayout();
          this.ResumeLayout(false);
+         this.PerformLayout();
 
       }
 
       #endregion
+
+      private System.Windows.Forms.MenuStrip _menuStrip;
+      private System.Windows.Forms.ToolStripMenuItem _fileMenu;
    }
 }
 
