1116d69ec13ed740ce1e6d2d361c6dc5ffafc82e
Alex Novak <alexwnovak@gmail.com>
2014-11-03 11:57:52 -0500

    Setting app background color to Workspace color

--- Paintr/Paintr/MainForm.Designer.cs
+++ Paintr/Paintr/MainForm.Designer.cs
@@ -63,6 +63,7 @@
          // 
          this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.BackColor = System.Drawing.SystemColors.AppWorkspace;
          this.ClientSize = new System.Drawing.Size(707, 459);
          this.Controls.Add(this._menuStrip);
          this.MainMenuStrip = this._menuStrip;
