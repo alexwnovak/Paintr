c874024fc80b14e92de4f44f29fd62342368f65a
Alex Novak <alexwnovak@gmail.com>
2014-11-06 16:03:28 -0500

    +AddLine method to draw a line on an image

--- Paintr/Paintr/ImageExtensions.cs
+++ Paintr/Paintr/ImageExtensions.cs
@@ -1,6 +1,15 @@
-﻿namespace Paintr
+﻿using System.Drawing;
+
+namespace Paintr
 {
    public static class ImageExtensions
    {
+      public static void AddLine( this Image image, Point startingPoint, Point endPoint, Pen pen )
+      {
+         using ( Graphics g = Graphics.FromImage( image ) )
+         {
+            g.DrawLine( pen, startingPoint, endPoint );
+         }
+      }
    }
 }
